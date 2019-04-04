cbf443242c2142626eb170d87dbf09aafc6a08c9
Gregory Setnik <gsetnik18@gmail.com>
2019-04-04 10:40:05 -0400

    spotify api call works, just need to set JSON object with album, etc.

--- liri.js
+++ liri.js
@@ -1,1 +1,28 @@
 require("dotenv").config();
+
+var axios =  require("axios");
+var Spotify = require("node-spotify-api");
+var moment = require("moment");
+var keys = require("./keys.js");
+const fs = require('fs');
+var spotify = new Spotify(keys.spotify);
+   
+
+var command = process.argv[2];
+var query = process.argv[3];
+
+var makeCall = function(cmd , qry){
+    if(cmd === 'spotify-this-song') {
+        console.log(qry);
+        //API query to Spotify
+        spotify.search({ type: 'track', query: qry }, function(err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+            }
+           
+          console.log(data); 
+          });
+    }
+};
+
+makeCall(command , query);
